fee7b4d9f2147547132948234e90ee66afc3533e
Semyon <ssshakurov@edu.hse.ru>
2021-04-19 14:31:42 +0300

    fix

--- 07-3a.c
+++ 07-3a.c
@@ -6,87 +6,92 @@
 #include <stdlib.h>
 #include <sys/sem.h>
 
-int increment(int semid, struct sembuf* buf) {
-    buf->sem_flg = 0;
-    buf->sem_num = 0;
-    buf->sem_op = 1;
-
-    return semop(semid, buf, 1);
-}
-
-int decrement(int semid, struct sembuf* buf) {
-    buf->sem_flg = 0;
-    buf->sem_op = -1;
-    buf->sem_num = 0;
-
-    return semop(semid, buf, 1);
-}
-
-
 int main()
 {
-  struct sembuf mybuf;
-  int semid;
-  int     *array;
-  int     shmid;
-  int     new = 1;
-  char    pathname[] = "07-3a.c";
-  key_t   key;
-  long    i;
+    struct sembuf sembuf;
+    struct sembuf mybuf;
+    int semid;
+    int* array;
+    int     shmid;
+    int     new = 1;
+    char    pathname[] = "07-3a.c";
+    key_t   key;
+    long    i;
 
-  if ((key = ftok(pathname,0)) < 0) {
-    printf("Can\'t generate key\n");
-    exit(-1);
-  }
-
-  if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0) {
-    if (errno != EEXIST) {
-      printf("Can\'t create shared memory\n");
-      exit(-1);
-    } else {
-      if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
-        printf("Can\'t find shared memory\n");
+    if ((key = ftok(pathname, 0)) < 0) {
+        printf("Can\'t generate key\n");
         exit(-1);
-      }
-      new = 0;
     }
-  }
 
-  if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
-    printf("Can't attach shared memory\n");
-    exit(-1);
-  }
+    sembuf.sem_op = 1;
+    sembuf.sem_num = 0;
+    sembuf.sem_flg = 0;
 
-  if ((semid = semget(key, 1, 0666)) < 0) {
-      printf("Семафор не найден, повтор¤ю попытку с флагом IPC CREAT...\n");
-      if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-          printf("Создать невозможно, аварийно завершаюсь.\n");
-          exit(-1);
-      }
-      printf("Успешно создан семафор!\n");
-      increment(semid, &mybuf);
-  }
+    mybuf.sem_op = -1;
+    mybuf.sem_num = 0;
+    mybuf.sem_flg = 0;
 
-  if (new) {
-    array[0] =  1;
-    array[1] =  0;
-    array[2] =  1;
-  } else {
-    decrement(semid, &mybuf);
-    array[0] += 1;
-    for(i=0; i<2000000000L; i++);
-    array[2] += 1;
-    increment(semid, &mybuf);
-  }
+    if ((semid = semget(key, 1, 0666)) < 0) {
+        printf("Semaphore doesnt exist\n");
+        if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+            printf("Cant create new semaphore");
+            exit(-1);
+        }
+        printf("Semaphore was created\n");
+        if (semop(semid, &sembuf, 1) < 0) {
+            printf("Cant wait for condition\n");
+            exit(-1);
+        }
+    }
 
-  printf
+    if ((shmid = shmget(key, 3 * sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+        if (errno != EEXIST) {
+            printf("Cant create shared memory\n");
+            exit(-1);
+        }
+        else {
+            if ((shmid = shmget(key, 3 * sizeof(int), 0)) < 0) {
+                printf("Cant find shared memory\n");
+                exit(-1);
+            }
+            new = 0;
+        }
+    }
+
+    if ((array = (int*)shmat(shmid, NULL, 0)) == (int*)(-1)) {
+        printf("Can't attach shared memory\n");
+        exit(-1);
+    }
+    
+    if (semop(semid, &mybuf, 1) < 0) {
+        printf("Can\'t wait for condition\n");
+        exit(-1);
+    }
+
+    if (new) {
+        array[0] = 1;
+        array[1] = 0;
+        array[2] = 1;
+    }
+    else {
+        array[0] += 1;
+        for (i = 0; i < 2000000000L; i++);
+        array[2] += 1;
+    }
+
+    printf
     ("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
-    array[0], array[1], array[2]);
+        array[0], array[1], array[2]);
 
-  if (shmdt(array) < 0) {
-    printf("Can't detach shared memory\n");
-    exit(-1);
-  }
+    if (semop(semid, &sembuf, 1) < 0) {
+        printf("Cant wait for condition\n");
+        exit(-1);
+    }
 
-  return 0;
+    if (shmdt(array) < 0) {
+        printf("Cant detach shared memory\n");
+        exit(-1);
+    }
+
+    return 0;
 }
